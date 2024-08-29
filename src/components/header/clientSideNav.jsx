fe2c18823dc2e13a8d91506ee617f61038922836
Matvey Zhigalka <mzhigalka@gmail.com>
2024-08-29 13:40:38 +0300

    added a choice of langs to the burger meny, fixed closing the menu on the mobile version and fixed errors in the burger menu on the Spanish version

--- src/components/header/clientSideNav.jsx
+++ src/components/header/clientSideNav.jsx
@@ -1,11 +1,14 @@
-'use client';
+"use client";
 
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from "react";
 import Link from "next/link";
 import styles from "./style.module.scss";
 
+import { LangChange } from "../ui/langChange";
+
 export const ClientSideNav = ({ translations }) => {
   const menuToggleRef = useRef(null);
+  const menuBoxRef = useRef(null);
 
   const closeMenu = () => {
     if (menuToggleRef.current) {
@@ -13,29 +16,43 @@ export const ClientSideNav = ({ translations }) => {
     }
   };
 
+  const handleClickOutside = (event) => {
+    if (
+      menuBoxRef.current &&
+      !menuBoxRef.current.contains(event.target) &&
+      menuToggleRef.current.checked
+    ) {
+      closeMenu();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav className={styles.hamburger_menu}>
       <input id={styles.menu__toggle} type="checkbox" ref={menuToggleRef} />
       <label className={styles.menu__btn} htmlFor={styles.menu__toggle}>
         <span></span>
       </label>
-      <ul className={styles.menu__box}>
+      <ul className={styles.menu__box} ref={menuBoxRef}>
         <div
-          className=""
+          className={styles.menu__box__inner}
           style={{
             marginLeft: 20,
             marginRight: 20,
             borderBottom: "1px solid #363636",
-            paddingBottom: 20, 
+            paddingBottom: 20,
+            paddingTop: 10,
             marginBottom: 36,
           }}
         >
-          <img
-            src="/assets/ui/logo.webp"
-            alt="logo"
-            width={192}
-            height={51}
-          />
+          <img src="/assets/ui/logo.webp" alt="logo" width={192} height={51} />
+          <LangChange padding="6px 8px 6px 12px" gap={3} />
         </div>
         <li>
           <Link href={"#"} className={styles.menu__item} onClick={closeMenu}>
@@ -43,22 +60,38 @@ export const ClientSideNav = ({ translations }) => {
           </Link>
         </li>
         <li>
-          <Link href={"#ourService"} className={styles.menu__item} onClick={closeMenu}>
+          <Link
+            href={"#ourService"}
+            className={styles.menu__item}
+            onClick={closeMenu}
+          >
             {translations.services}
           </Link>
         </li>
         <li>
-          <Link href={"/interiorAll"} className={styles.menu__item} onClick={closeMenu}>
+          <Link
+            href={"/interiorAll"}
+            className={styles.menu__item}
+            onClick={closeMenu}
+          >
             {translations.projects}
           </Link>
         </li>
         <li>
-          <Link href={"#interiorDesing"} className={styles.menu__item} onClick={closeMenu}>
+          <Link
+            href={"#interiorDesing"}
+            className={styles.menu__item}
+            onClick={closeMenu}
+          >
             {translations.interiorDesign}
           </Link>
         </li>
         <li>
-          <Link href={"#aboutUs"} className={styles.menu__item} onClick={closeMenu}>
+          <Link
+            href={"#aboutUs"}
+            className={styles.menu__item}
+            onClick={closeMenu}
+          >
             {translations.aboutUs}
           </Link>
         </li>
