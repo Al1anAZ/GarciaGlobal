eddf0559a6c0da81c2f0a19430456f2ac31f8700
madach121212 <maxik.dach212121@gmail.com>
2024-09-09 21:01:00 +0200

    changed arrowback

--- src/components/backToHome/index.jsx
+++ src/components/backToHome/index.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+"use client";  
+
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import initTranslations from "../../app/i18n";
-import Link from "next/link";
 import styles from "./style.module.scss";
 import { ArrowBack } from "../ui/icons/arrowBack";
 
 const i18nNamespaces = ["backToHome"];
-export const BackToHome = async ({ locale }) => {
-  const { t } = await initTranslations(locale, i18nNamespaces);
+
+export const BackToHome = ({ locale }) => {
+  const router = useRouter();   
+  const [t, setT] = useState(() => (key) => key);  
+ 
+  useEffect(() => {
+    const loadTranslations = async () => {
+      const { t } = await initTranslations(locale, i18nNamespaces);
+      setT(() => t);  
+    };
+
+    loadTranslations();
+  }, [locale]);
+
   return (
-    <Link className={styles.blackToHome} href="/">
+    <button
+      className={styles.blackToHome}
+      onClick={() => router.back()}  
+    >
       <ArrowBack color="white" width={10} height={10} />
       {t("back")}
-    </Link>
+    </button>
   );
 };
